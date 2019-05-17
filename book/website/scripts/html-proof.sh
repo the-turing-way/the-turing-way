bcbf5ac89979fa8bd90b6d118c05cb3dc2840fb5
Tania Allard <taniar.allard@gmail.com>
2019-05-17 14:56:24 +0100

    Ignore  some internal urls

--- book/website/scripts/html-proof.sh
+++ book/website/scripts/html-proof.sh
@@ -2,4 +2,4 @@
 set -e # halt script on error
 
 make site 
-bundle exec htmlproofer  --assume-extension ./_site --disable-external --only_4xx
+bundle exec htmlproofer  --assume-extension ./_site --disable-external --only_4xx htmlproofer --url-ignore "/apple-touch", "js-page__nav__next"
