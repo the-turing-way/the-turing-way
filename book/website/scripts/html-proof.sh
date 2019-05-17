1799bb845930497a0bdfa627e92b22856ab3b717
Tania Allard <taniar.allard@gmail.com>
2019-05-17 15:18:36 +0100

    Last setup for html proofer

--- book/website/scripts/html-proof.sh
+++ book/website/scripts/html-proof.sh
@@ -2,8 +2,9 @@
 set -e # halt script on error
 
 make site 
-bundle exec htmlproofer  --assume-extension ./_site \
---url-ignore "/\/apple-touch*.*/" \
+bundle exec htmlproofer --allow-hash-href \
+--assume-extension ./_site \
+--url-ignore "/\/apple-touch*.*/,/\/images/logo/favicon.ico/,/#*/" \
 --disable-external \
---only_4xx \
---allow-hash-href 
+--only_4xx 
+
