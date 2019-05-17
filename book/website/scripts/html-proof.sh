c620b1e13d74c550b5c53f90c1a83861ff657d22
Tania Allard <taniar.allard@gmail.com>
2019-05-17 15:09:46 +0100

    Allow # refs

--- book/website/scripts/html-proof.sh
+++ book/website/scripts/html-proof.sh
@@ -2,4 +2,8 @@
 set -e # halt script on error
 
 make site 
-bundle exec htmlproofer  --assume-extension ./_site --url-ignore "/\/apple-touch*.*/" --disable-external --only_4xx 
+bundle exec htmlproofer  --assume-extension ./_site \
+--url-ignore "/\/apple-touch*.*/" \
+--disable-external \
+--only_4xx \
+--allow-hash-href 
