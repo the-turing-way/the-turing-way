c2f0031f19b04591799b5cbf1757274cc8ca0c5d
tony <tony@tony.tc>
2020-02-05 14:30:29 +0000

    Add multiligual make

---
+++ book/website/scripts/multilingual_make.sh
@@ -0,0 +1,11 @@
+sed 's+content_folder_name: ../content+content_folder_name: ../content/locale/+g' _config.yml > _config-locale.yml
+cat ../content/po/LINGUAS | while read lang;
+do
+    echo ${lang}
+    sed "s/url: /url: ${lang}\//g" _data/toc.yml.bak > _data/toc.yml
+    jupyter-book build ./ --overwrite --config _config-locale.yml
+    find _build/${lang}/ -name *.md -exec sed -i s+../figures+../../figures+g {} +
+    bundle exec jekyll build
+    rm -r _build/${lang}
+done
+cp _data/toc.yml.bak _data/toc.yml
