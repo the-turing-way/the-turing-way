5c6623a1a6fd3f0c049f086e92707310c0dfb872
Sarah Gibson <sgibson@turing.ac.uk>
2020-02-21 11:57:14 +0000

    Refactor no-bad-latin.py, add functions, ignore image files

--- tests/no-bad-latin.py
+++ tests/no-bad-latin.py
@@ -9,37 +9,112 @@ directory_to_check = os.path.join(script_dir, "../book/content/")
 
 
 def remove_comments(text_string):
-    p = re.sub("(?s)<!--(.*?)-->", "", text_string)
-    return p
+	"""Function to omit  html comment identifiers in a text string using
+	regular expression matches
+
+	Arguments:
+		text_string {string} -- The text to be matched
+
+	Returns:
+		{string} -- The input text string with html comments removed
+	"""
+	p = re.sub("(?s)<!--(.*?)-->", "", text_string)
+	return p
 
 
 def get_lines(text_string, sub_string):
-    lines = [line for line in text_string.split("\n") if sub_string in line]
-    return lines
+	"""Get individual lines in a text file
+
+	Arguments:
+		text_string {string} -- The text string to test
+		sub_string {string} -- The conditional string to perform splitting on
+
+	Returns:
+		{list} -- A list of split strings
+	"""
+	lines = [line for line in text_string.split("\n") if sub_string in line]
+	return lines
 
 
-bad_latin = ["i.e.", "e.g.", "e.t.c.", " etc", " ie"]
-fails = {}  # keys = filenames, values = (latin_type, line)
-for root_dir, _, file_names in os.walk(directory_to_check):
-    for file_name in file_names:
-        file = open(os.path.join(root_dir, file_name), encoding="utf8", errors="ignore")
-        text = file.read()
-        text = remove_comments(text)
-        for latin_type in bad_latin:
-            if latin_type in text.lower():
-                for line in get_lines(text.lower(), latin_type):
-                    try:
-                        fails[os.path.abspath(file.name)].append((latin_type, line))
-                    except:
-                        fails[os.path.abspath(file.name)] = [(latin_type, line)]
+def get_files(directory):
+	"""Get a list of files to be checked. Ignores image files.
+
+	Arguments:
+		directory {string} -- The directory containing the files to check
+
+	Returns:
+		{list} -- List of files to check
+	"""
+	files = []
+	filetypes_to_ignore = (".png", ".jpg")
+
+	for rootdir, _, filenames in os.walk(directory):
+		for filename in filenames:
+			if not filename.endswith(filetypes_to_ignore):
+				files.append(os.path.join(rootdir, filename))
+
+	return files
 
 
-if len(fails.keys()) != 0:
-    error_message = "Bad latin found in the following files:\n\n"
-    for file_loc in fails.keys():
-        error_message += file_loc + "\n"
-        for instance_of_fail in fails[file_loc]:
-            latin_type, line = instance_of_fail
-            error_message += latin_type + '\t found in line \t"' + line + '"\n'
-        error_message += "\n"
-    raise Exception(error_message)
+def read_and_check_files(files):
+	"""Function to read in files, remove html comments and check for bad latin
+	phrases
+
+	Arguments:
+		files {list} -- List of filenames to be checked
+
+	Returns:
+		{dict} -- Dictionary: Top level keys are absolute filepaths to files
+		          that failed the check. Each of these has two keys:
+				  'latin_type' containing the unwanted latin phrase, and 'line'
+				  containing the offending line.
+	"""
+	failing_files = {}
+	bad_latin = ["i.e.", "e.g.", "e.t.c.", " etc", " ie"]
+
+	for filename in files:
+		with open(filename, encoding="utf8", errors="ignore") as f:
+			text = f.read()
+		text = remove_comments(text)
+
+		for latin_type in bad_latin:
+			if latin_type in text.lower():
+				lines = get_lines(text.lower(), latin_type)
+				for line in lines:
+					failing_files[os.path.abspath(filename)] = {"latin_type": latin_type, "line": line}
+
+	return failing_files
+
+
+def construct_error_message(files_dict):
+	"""Function to construct an error message pointing out where bad latin
+	phrases appear in lines of text
+
+	Arguments:
+		files_dict {dictionary} -- Dictionary of failing files containing the
+		                           bad latin phrases and offending lines
+
+	Returns:
+		{string} -- The error message to be raised
+	"""
+	error_message = ["Bad latin found in the following files:\n"]
+
+	for file in files_dict.keys():
+		error_message.append(
+			f"{file}:\t{files_dict[file]['latin_type']}\tfound in line\t[{files_dict[file]['line']}]"
+		)
+
+	return "\n".join(error_message)
+
+
+def main():
+	files = get_files(directory_to_check)
+	failing_files = read_and_check_files(files)
+
+	if bool(failing_files):
+		error_message = construct_error_message(failing_files)
+		raise Exception(error_message)
+
+
+if __name__ == "__main__":
+	main()
