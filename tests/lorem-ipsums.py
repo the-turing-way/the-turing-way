fb033b1c7560713b37aeefa61ad800688e802a60
Sarah Gibson <sgibson@turing.ac.uk>
2020-02-21 10:11:44 +0000

    Black formatting

--- tests/lorem-ipsums.py
+++ tests/lorem-ipsums.py
@@ -8,7 +8,7 @@ directory_to_check = os.path.join(script_dir, "../book/content/")
 
 
 def remove_comments(text_string):
-	"""Function to omit  html comment identifiers in a text string using
+    """Function to omit  html comment identifiers in a text string using
 	regular expression matches
 
 	Arguments:
@@ -22,27 +22,31 @@ def remove_comments(text_string):
 
 
 def main():
-	"""Main function
+    """Main function
 	Checks for rogue "lorem ipsum"s in book content files and removes html
 	comment identifiers from the files.
 
 	Raises:
 		Exception: Raises an exception if "lorem ipsum" is found in a book
-		           content file
+					content file
 	"""
-	failed = []
-	for root_dir, _, file_names in os.walk(directory_to_check):
-		for file_name in file_names:
-			file = open(os.path.join(root_dir, file_name), encoding="utf8", errors="ignore")
-			text = file.read()
-			text = remove_comments(text)
-			if "lorem ipsum" in text.lower():
-				failed.append(file.name)
+    failed = []
+    for root_dir, _, file_names in os.walk(directory_to_check):
+        for file_name in file_names:
+            file = open(
+                os.path.join(root_dir, file_name), encoding="utf8", errors="ignore"
+            )
+            text = file.read()
+            text = remove_comments(text)
+            if "lorem ipsum" in text.lower():
+                failed.append(file.name)
 
-	if len(failed) != 0:
-		error_message = '"Lorem ipsum"s found in the following files:\n' + "\n".join(failed)
-		raise Exception(error_message)
+    if len(failed) != 0:
+        error_message = '"Lorem ipsum"s found in the following files:\n' + "\n".join(
+            failed
+        )
+        raise Exception(error_message)
 
 
 if __name__ == "__main__":
-	main()
+    main()
