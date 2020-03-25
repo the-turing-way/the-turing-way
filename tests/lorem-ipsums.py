e30c11250cf8d415762adfbd5e234e49903c7d74
Sarah Gibson <sgibson@turing.ac.uk>
2020-03-25 18:14:24 +0000

    Edit lorem-ipsums.py to take list of files as input

--- tests/lorem-ipsums.py
+++ tests/lorem-ipsums.py
@@ -2,9 +2,23 @@
 import os
 import re
 import sys
+import argparse
 
 script_dir = os.path.dirname(__file__)
 directory_to_check = os.path.join(script_dir, "../book/content/")
+
+
+def parse_args():
+    """Parse an optional list of files to check"""
+    parser = argparse.ArgumentParser(
+        description="Parse an optional list of files to check"
+    )
+
+    parser.add_argument(
+        "--files", nargs="+", help="Individual files to check (can be many)"
+    )
+
+    return parser.parse_args()
 
 
 def remove_comments(text_string):
@@ -30,16 +44,27 @@ def main():
 		Exception: Raises an exception if "lorem ipsum" is found in a book
 					content file
 	"""
+    args = parse_args()
     failed = []
-    for root_dir, _, file_names in os.walk(directory_to_check):
-        for file_name in file_names:
-            file = open(
-                os.path.join(root_dir, file_name), encoding="utf8", errors="ignore"
-            )
+
+    if args.files:
+        for file_name in args.files:
+            file = open(os.path.join(os.pardir, file_name), encoding="utf8", errors="ignore")
             text = file.read()
             text = remove_comments(text)
             if "lorem ipsum" in text.lower():
                 failed.append(file.name)
+
+    else:
+        for root_dir, _, file_names in os.walk(directory_to_check):
+            for file_name in file_names:
+                file = open(
+                    os.path.join(root_dir, file_name), encoding="utf8", errors="ignore"
+                )
+                text = file.read()
+                text = remove_comments(text)
+                if "lorem ipsum" in text.lower():
+                    failed.append(file.name)
 
     if len(failed) != 0:
         error_message = '"Lorem ipsum"s found in the following files:\n' + "\n".join(
