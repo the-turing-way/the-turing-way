d2b8fc1d0688390afdf003b9c52b2359934ca92b
Tony Yang <tony@tony.tc>
2020-04-15 18:29:46 +0100

    Modify multilingual_make to use cached toc

--- book/website/scripts/multilingual_make.sh
+++ book/website/scripts/multilingual_make.sh
@@ -8,23 +8,23 @@ cd ../website
 # the translated Markdown files will be
 sed 's+content_folder_name: ../content+content_folder_name: ../content/locale/+g' _config.yml > _config-locale.yml
 # Make backup of the original table of content
-cp _data/toc.yml _data/toc.yml.bak
+cp ../content/po/toc.yml ../content/po/toc.yml.bak
 cat ../content/po/LINGUAS | while read lang;
 do
     echo ${lang}
     # Make language specific table of content
-    sed "s/url: /url: ${lang}\//g" _data/toc.yml.bak > _data/toc.yml
+    sed "s/url: /url: ${lang}\//g" ../content/po/toc.yml.bak > ../content/po/toc.yml
     # Use the locale config to build each language version of the book
-    jupyter-book build ./ --overwrite --config _config-locale.yml
+    jupyter-book build ./ --overwrite --config _config-locale.yml --toc ../content/po/toc.yml
     # Change the path to figures directory one level higher
     # This will use the figures from the English version
     # Should a translated figure exist, it will reside in a folder different
     # from the sed match here
-    find _build/${lang}/ -name *.md -exec sed -i "s+\.\./figures+../../figures+g" {} +
+    find _build/${lang}/ -name *.md -exec sed -ie "s+\.\./figures+../../figures+g" {} +
     bundle exec jekyll build
     # Remove the translated Markdown from the _build directory otherwise jekyll 
     # will build those files again
     rm -r _build/${lang}
 done
 # Restore the table of content to the oiginal version
-cp _data/toc.yml.bak _data/toc.yml
+cp ../content/po/toc.yml.bak ../content/po/toc.yml
