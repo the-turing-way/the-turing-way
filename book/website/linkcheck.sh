1018ac4384e49b124d155943e1e27303071ad1d8
Martina G. Vilas <martinagonzalezvilas@gmail.com>
2020-05-11 14:33:02 +0200

    change path to config

--- book/website/linkcheck.sh
+++ book/website/linkcheck.sh
@@ -2,4 +2,4 @@
   # halt script on error
   set -e
   # Build locally, deleting any existing doctrees, and then check links
-  sphinx-build -E -W -b linkcheck ./book/website ./book/website/_build
+  sphinx-build -E -W -b linkcheck -c ./book/website/_config.py ./book/website ./book/website/_build
