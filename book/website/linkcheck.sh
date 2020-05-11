901975872b3dc439b623e23b02d3c742000b26dc
Martina G. Vilas <martinagonzalezvilas@gmail.com>
2020-05-11 21:47:34 +0200

    remove linkcheck

--- book/website/linkcheck.sh
+++
@@ -1,5 +0,0 @@
-  #!/usr/bin/env bash
-  # halt script on error
-  set -e
-  # Build locally, deleting any existing doctrees, and then check links
-  sphinx-build -E -W -b linkcheck -c ./book/website/_config.py ./book/website ./book/website/_build
