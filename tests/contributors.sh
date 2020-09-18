683eb15c21b5ff07841ba246f30c95c7e08d0ea0
malvikasharan <malvikasharan@gmail.com>
2020-09-18 10:56:26 +0100

    update file names

--- tests/contributors.sh
+++ tests/contributors.sh
@@ -2,18 +2,18 @@
 # Copy the latest contributors.md file content to afterword
 # this will publish the contributors file online in The Turing Way book
 all_contributors='../../README.md' # Path to the main README file of The Turing Way
-contributor_highlights='../../contributors.md'
-contributor_records='../book/website/afterword/contributor-records.md'
+contributors_highlight='../../contributors.md'
+contributors_record='../book/website/afterword/contributors-record.md'
 
 # Copy everything from the contributors highlight
-echo '(aw-contributors-record-highlights)=' > $contributor_record
-cat $contributor_highlights >> $contributor_record
+echo '(aw-contributors-record-highlights)=' > $contributors_record
+cat $contributors_highlight >> $contributors_record
 
 # # Get linenumber where all contributors list starts
 line_num=$(grep -n '## Contributors' $all_contributors | cut -d: -f1)
-echo '\n(aw-contributors-record-contributors)=' >> $contributor_record
+echo '\n(aw-contributors-record-contributors)=' >> $contributors_record
 
 tail -n +"$line_num" "$all_contributors" | while read line;
 do
-  echo $line >> $contributor_record
+  echo $line >> $contributors_record
 done
