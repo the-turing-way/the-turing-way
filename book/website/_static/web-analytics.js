918386b0c619f5ee24e788e18dcb7a7dd8537509
Malvika Sharan <malvikasharan@gmail.com>
2021-02-24 16:30:39 +0000

    Update web-analytics.js

--- book/website/_static/web-analytics.js
+++ book/website/_static/web-analytics.js
@@ -6,10 +6,10 @@ var _paq = window._paq = window._paq || [];
 /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
 _paq.push(['trackPageView']);
 _paq.push(['enableLinkTracking']);
-(function () {
-    var u="https://theturingwaynetlify.matomo.cloud/";
-    _paq.push(['setTrackerUrl', u+'matomo.php']);
-    _paq.push(['setSiteId', '3']);
-    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-    g.type='text/javascript'; g.async=true; g.src='//cdn.matomo.cloud/theturingwaynetlify.matomo.cloud/matomo.js'; s.parentNode.insertBefore(g,s);
+(function() {
+  var u="https://theturingwaynetlify.matomo.cloud/";
+  _paq.push(['setTrackerUrl', u+'matomo.php']);
+  _paq.push(['setSiteId', '3']);
+  var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+  g.type='text/javascript'; g.async=true; g.src='//cdn.matomo.cloud/theturingwaynetlify.matomo.cloud/matomo.js'; s.parentNode.insertBefore(g,s);
 })();
