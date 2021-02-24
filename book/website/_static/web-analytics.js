6059e0b903bf588d8854de1c84157c94b8edc8bc
Malvika Sharan <malvikasharan@gmail.com>
2021-02-24 14:35:04 +0000

    Update web-analytics.js

--- book/website/_static/web-analytics.js
+++ book/website/_static/web-analytics.js
@@ -7,9 +7,9 @@ var _paq = window._paq = window._paq || [];
 _paq.push(['trackPageView']);
 _paq.push(['enableLinkTracking']);
 (function () {
-    var u = "//https://theturingwaynetlify.matomo.cloud/";
+    var u = "https://theturingwaynetlify.matomo.cloud/";
     _paq.push(['setTrackerUrl', u + 'matomo.php']);
     _paq.push(['setSiteId', '1']);
     var d = document, g = d.createElement('script'), s = d.getElementsByTagName('script')[0];
-    g.type = 'text/javascript'; g.async = true; g.src = u + 'matomo.js'; s.parentNode.insertBefore(g, s);
+    g.type = 'text/javascript'; g.async = true; g.src = '//cdn.matomo.cloud/theturingwaynetlify.matomo.cloud/matomo.js'; s.parentNode.insertBefore(g, s);
 })();
