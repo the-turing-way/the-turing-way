8ff7a27daa03ba94c67accc968e10c74cae3e2e4
Malvika Sharan <malvikasharan@gmail.com>
2021-02-24 17:39:23 +0000

    Update web-analytics.js

--- book/website/_static/web-analytics.js
+++ book/website/_static/web-analytics.js
@@ -2,5 +2,5 @@
  * This file contains the tracking code that allows analytics to work on The Turing Way using Matomo
 */
 
-<script async defer data-domain="deploy-preview-1749--the-turing-way.netlify.app" src="https://plausible.io/js/plausible.js"></script>
+<script async defer data-domain="deploy-preview-1750--the-turing-way.netlify.app" src="https://plausible.io/js/plausible.js"></script>
 
