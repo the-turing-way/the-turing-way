1c39e296bf7ced0d544e31d0033408dbab1f1c94
Malvika Sharan <malvikasharan@gmail.com>
2021-07-26 17:25:37 +0100

    Update tests/contributors.sh
    Co-authored-by: Sarah Gibson <44771837+sgibson91@users.noreply.github.com>

--- tests/contributors.sh
+++ tests/contributors.sh
@@ -14,7 +14,7 @@ cat $contributor_highlights >> $contributors_record
 # # Get linenumber where all contributors list starts
 echo '\n(contributors-record-all)=' >> $contributors_record
 echo '\n# All Contributors\n' >> $contributors_record
-echo '\n✨Using [all-contributors](https://allcontributors.org/) specification, _The Turing Way_ Recognises all contributors, not just the ones who push code. ✨' >> $contributors_record
+echo '\n✨Using [all-contributors](https://allcontributors.org/) specification, _The Turing Way_ recognises all contributors, not just the ones who push code. ✨' >> $contributors_record
 line_num=$(grep -n '## Contributors' $all_contributors | cut -d: -f1)
 echo '\n(contributors-record-contributors)=' >> $contributors_record
 
