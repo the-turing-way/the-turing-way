376b5a0230eeff19f8b9ef0bf95ed98fbaefbbfb
Malvika Sharan <malvikasharan@gmail.com>
2024-04-15 18:11:23 +0100

    Add files via upload

---
+++ project_management/quarterly_reports/book-stats.md
@@ -0,0 +1,42 @@
+Book stats for chapters and subchapters, generated on: 2024-04-15 18:10:07.3N
+
+Guide: reproducible-research
+Number of chapters
+17
+Number of subchapters
+116
+
+Guide: project-design
+Number of chapters
+11
+Number of subchapters
+45
+
+Guide: communication
+Number of chapters
+15
+Number of subchapters
+57
+
+Guide: collaboration
+Number of chapters
+19
+Number of subchapters
+62
+
+Guide: ethical-research
+Number of chapters
+12
+Number of subchapters
+28
+
+Guide: community-handbook
+Number of chapters
+17
+Number of subchapters
+70
+
+Total number of chapters:
+91
+Total number of subchapters:
+378
---
+++ project_management/quarterly_reports/count-chapters.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# run this script locally in the 'website' folder
+# Indicate the date and time for last stat generation
+current_time=$(date +"%Y-%m-%d %H:%M:%S.%3N")
+echo "Book stats for chapters and subchapters, \
+generated on: $current_time" > book-stats.md
+
+path="../../book/website/"
+# Sum up chapters and subchapter
+total_chapters=0
+total_subchapters=0
+
+# list of guides
+guide_list=(reproducible-research project-design \
+communication collaboration ethical-research \
+community-handbook)
+
+# loop over guide list
+for guide in "${guide_list[@]}";do
+    echo ${guide}
+# count chapters in each guide
+    echo "\nGuide: $guide"  >> book-stats.md
+    echo "Number of chapters"
+    echo "Number of chapters"  >> book-stats.md
+    chapters=$(find "${path}${guide}" -maxdepth 1 -type f | cut -d/ -f2 | sort | wc -l)
+    echo $chapters >> book-stats.md
+    echo chapters=$((chapters + $chapters))
+    total_chapters=$((total_chapters + $chapters))
+
+# count subchapters in each guide
+    echo "Number of subchapters"
+    echo "Number of subchapters"  >> book-stats.md
+    subchapters=$(find "${path}${guide}" -type f | cut -d/ -f2 | sort | wc -l)
+    echo $subchapters >> book-stats.md
+    echo subchapters=$((subchapters + $subchapters))
+    total_subchapters=$((total_subchapters + $subchapters))
+
+done
+
+# Sum all chapters and subchapters from all guides
+echo "\nTotal number of chapters:" >>  book-stats.md
+echo $total_chapters >>  book-stats.md
+echo "Total number of subchapters:" >>  book-stats.md
+echo $total_subchapters >>  book-stats.md
